3b9ec20cf88385334992ca058a20364351e9f85f
sue-lima <suelen.ferreira94@gmail.com>
2023-06-27 19:39:38 -0300

    initial sketch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,170 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import Image from 'next/image'
+import moon from '../assets/waxingGibbous.png'
+import sunrise from '../assets/sunrise.png'
+import sunset from '../assets/sunset.png'
+import sun from '../assets/sun.png'
+import sunny from '../assets/sunny.png'
+import storm from '../assets/storm.png'
+import { FaSearch, FaLocationArrow, FaWind, FaPercent, FaCloudRain , FaMoon, FaSun} from 'react-icons/fa'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className={styles.bar}>
+        <div className={styles.dark} title="Dark&Light mode">
+          <i className={styles.toggle}></i>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.searchBar}>
+        <div className={styles.searchInput}>
+          <FaSearch />
+          <input type="text" placeholder='Search for a City'/>
+        </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className={styles.secTemp}>
+        <div className={styles.containerLeft}>
+          <div className={styles.location}>
+            <FaLocationArrow />
+            <strong>São Paulo, SP</strong>
+          </div>
+          <div className={styles.temp}>
+            <h1>21°</h1>
+            <p>16° <span>9°</span></p>
+          </div>
+          <div className={styles.statist}>
+            <div className={styles.info}>
+              <FaWind />
+              <p>Wind</p>
+              <h5>17 <span>km/h</span></h5>
+            </div>
+            <div className={styles.info}>
+              <FaPercent />
+              <p>Precip</p>
+              <h5>0 <span>%</span></h5>
+            </div>
+            <div className={styles.info}>
+              <FaCloudRain />
+              <p>Rain</p>
+              <h5>17 <span>%</span></h5>
+            </div>
+          </div>
+        </div>
+        <div className={styles.containerRight}>
+            <div className={styles.sunTime}>
+              <div className={styles.title}>
+                <FaSun />
+                <strong>Sun Time</strong>
+              </div>
+              <div className={styles.time}>
+                <p>02:17 pm</p>
+              </div>
+              <div className={styles.sun}>
+                <div className={styles.sunrise}>
+                  <Image src={sunrise} alt='Sunrise' width={65} height={65}/>
+                  <p>05:45</p>
+                </div>
+                <div className={styles.sunset}>
+                  <Image src={sunset} alt='Sunset' width={65} height={65}/>
+                  <p>17:48</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.moonPhase}>
+              <div className={styles.title}>
+                <FaMoon />
+                <strong>Moon phase</strong>
+              </div>
+              <Image src={moon} alt='MoonPhase' width={147} height={151}/>
+              <p>Waxing Gibbous</p>
+            </div>
+            <div className={styles.forecast}>
+              <div className={styles.days}>
+                <div className={styles.day}>
+                  <strong>Wednesday</strong>
+                  <Image src={sunny} alt='Sunset' width={65} height={65}/>
+                  <p>26° <span>21°</span></p>
+                </div>
+              </div>
+              <div className={styles.days}>
+                <div className={styles.day}>
+                  <strong>Thursday</strong>
+                  <Image src={sun} alt='Sunset' width={65} height={65}/>
+                  <p>22° <span>18°</span></p>
+                </div>
+              </div>
+              <div className={styles.days}>
+                <div className={styles.day}>
+                  <strong>Friday</strong>
+                  <Image src={storm} alt='Sunset' width={65} height={65}/>
+                  <p>19° <span>13°</span></p>
+                </div>
+              </div>
+            </div>
+        </div>
+      </section>
+      <section className={styles.weekly}>
+        <div className={styles.week}>
+          <div className={styles.forecast}>
+              <div className={styles.days}>
+                <div className={styles.day}>
+                  <strong>Wednesday</strong>
+                  <Image src={sunny} alt='Sunset' width={65} height={65}/>
+                  <p>26° <span>21°</span></p>
+                </div>
+              </div>
+              <div className={styles.days}>
+                <div className={styles.day}>
+                  <strong>Thursday</strong>
+                  <Image src={sun} alt='Sunset' width={65} height={65}/>
+                  <p>22° <span>18°</span></p>
+                </div>
+              </div>
+              <div className={styles.days}>
+                <div className={styles.day}>
+                  <strong>Friday</strong>
+                  <Image src={storm} alt='Sunset' width={65} height={65}/>
+                  <p>19° <span>13°</span></p>
+                </div>
+              </div>
+              <div className={styles.days}>
+                <div className={styles.day}>
+                  <strong>Saturday</strong>
+                  <Image src={sunny} alt='Sunset' width={65} height={65}/>
+                  <p>26° <span>21°</span></p>
+                </div>
+              </div>
+              <div className={styles.days}>
+                <div className={styles.day}>
+                  <strong>Sunday</strong>
+                  <Image src={sunny} alt='Sunset' width={65} height={65}/>
+                  <p>26° <span>21°</span></p>
+                </div>
+              </div>
+              <div className={styles.days}>
+                <div className={styles.day}>
+                  <strong>Monday</strong>
+                  <Image src={storm} alt='Sunset' width={65} height={65}/>
+                  <p>19° <span>13°</span></p>
+                </div>
+              </div>
+              <div className={styles.days}>
+                <div className={styles.day}>
+                  <strong>Tuesday</strong>
+                  <Image src={storm} alt='Sunset' width={65} height={65}/>
+                  <p>19° <span>13°</span></p>
+                </div>
+              </div>
+              <div className={styles.days}>
+                <div className={styles.day}>
+                  <strong>Wednesday</strong>
+                  <Image src={storm} alt='Sunset' width={65} height={65}/>
+                  <p>19° <span>13°</span></p>
+                </div>
+              </div>
+            </div>
+        </div>
+      </section>
     </main>
   )
 }
